d0391c52d45522c54ba783b6a1f1d3a0a90bbbfa
mrralphpierre <mrralphpierre@gmail.com>
2021-08-27 21:42:03 -0400

    added employee js and test js files, installedjest to run the test

---
+++ employee-test.js
@@ -0,0 +1,29 @@
+const { expect, test } = require("@jest/globals");
+const Employee = require("./employees");
+const employee = new Employee('ralph', '8054264', 'mrralphpierre@gmail.com');
+
+it('test if we can get the constrcutor values for the employee object', () => {
+    expect(employee.name).toBe('ralph');
+    expect(employee.id).toBe('8054264');
+    expect(employee.email).toBe('mrralphpierre@gmail.com');
+});
+
+it('test if we can get the name from the getName() method' ,() => {
+    expect(employee.getName).toBe('ralph');
+
+});
+
+it('test if we can get the name from the getId() method' ,() => {
+    expect(employee.getID).toBe('8054264');
+
+});
+
+it('test if we can get the name from the getEmail() method' ,() => {
+    expect(employee.getEmail).toBe('mrralphpierre@gmail.com');
+
+});
+
+it('test if we can get the name from the getRole() method' ,() => {
+    expect(employee.getRole).toBe('Employee');
+
+});
